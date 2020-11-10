f3fc87687c311aff6a1cea69e41a41d9b595c72a
Mike <mike@Taylors-MacBook-Air.local>
2020-11-09 21:33:59 -0500

    implemented "On Enter" for login

--- src/app/pages/account/login/login.ts
+++ src/app/pages/account/login/login.ts
@@ -1,6 +1,5 @@
-import { Component } from "@angular/core";
-import { FormBuilder, FormGroup, NgForm, Validators } from "@angular/forms";
-import { ActivatedRoute, Router } from "@angular/router";
+import { Component, HostListener } from "@angular/core";
+import { Router } from "@angular/router";
 
 import { UserData } from "@app/providers/user-data";
 
@@ -8,38 +7,28 @@ import { UserOptions } from "@app/interfaces/user-options";
 import { AccountService, AlertService } from "@app/_services";
 import { first } from "rxjs/operators";
 
+
 @Component({
   selector: "page-login",
   templateUrl: "login.html",
   styleUrls: ["./login.scss"],
 })
 export class LoginPage {
-  form: FormGroup;
+  // Event listener for enter button for easy login!!!
+  @HostListener("document:keydown.enter", ["$event"]) async onKeydownHandler(
+    evt: KeyboardEvent
+  ) {
+    await this.onLogin();
+  }
   loading = false;
   submitted = false;
-
   login: UserOptions = { email: "", password: "" };
-
   constructor(
-    private formBuilder: FormBuilder,
     public userData: UserData,
     public router: Router,
-    private route: ActivatedRoute,
     private accountService: AccountService,
-    private alertService:AlertService
+    private alertService: AlertService
   ) {}
-
-  async ngOnInit() {
-    this.form = this.formBuilder.group({
-      email: ["", [Validators.required, Validators.email]],
-      password: ["", Validators.required],
-    });
-  }
-
-  // convenience getter for easy access to form fields
-  get f() {
-    return this.form.controls;
-  }
 
   async onSignup() {
     await this.router.navigateByUrl("/signup");
@@ -49,29 +38,36 @@ export class LoginPage {
     await this.router.navigateByUrl("/forgot-password");
   }
 
-  async onLogin(form: NgForm) {
-    this.alertService.presentLoading("Logging In...",1500);
+  async onLogin() {
+    await this.alertService.presentLoading("Logging In...", 1500);
     this.submitted = true;
 
-    if (form.valid) {
-      this.loading = true;
-      this.accountService
-        .login(this.login.email, this.login.password)
-        .pipe(first())
-        .subscribe({
-          next:async () => {
-            // get return url from query parameters or default to home page
-            await this.userData.login(this.login.email);
-            // redirect to home page when you login
-            await this.router.navigateByUrl("/home");
-            await this.alertService.createToastAlert("Log In Sucessful","success",2000);
-          },
-          error:async (error) => {
-            await this.alertService.createToastAlert("Log In Failed, Please Check That Your Email & Password Is Correct.","danger",3000);
-            this.loading = false;
-            return;
-          },
-        });
-    }
+    this.loading = true;
+    this.accountService
+      .login(this.login.email, this.login.password)
+      .pipe(first())
+      .subscribe({
+        next: async () => {
+          // get return url from query parameters or default to home page
+          await this.userData.login(this.login.email);
+          // redirect to home page when you login
+          await this.router.navigateByUrl("/home");
+          await this.alertService.createToastAlert(
+            "Log In Sucessful",
+            "success",
+            2000
+          );
+          this.loading = false;
+        },
+        error: async (error) => {
+          await this.alertService.createToastAlert(
+            "Log In Failed, Please Check That Your Email & Password Is Correct.",
+            "danger",
+            3000
+          );
+          this.loading = false;
+          return;
+        },
+      });
   }
 }
