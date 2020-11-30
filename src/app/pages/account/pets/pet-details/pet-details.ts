16896acc6a53c56cb8e84ab88049e46f9d783e5d
Mike <mike@Taylors-MacBook-Air.local>
2020-11-29 22:09:47 -0500

    starting on uploads functionality

--- src/app/pages/account/pets/pet-details/pet-details.ts
+++ src/app/pages/account/pets/pet-details/pet-details.ts
@@ -1,11 +1,24 @@
-import { Component } from "@angular/core";
+import { Component , OnInit, ChangeDetectorRef  } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { InAppBrowser } from "@ionic-native/in-app-browser/ngx";
 import { AccountService, AlertService, PetService } from "@app/_services";
 import { AlertController } from "@ionic/angular";
 import { first } from "rxjs/operators";
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
 
+import { ActionSheetController, ToastController, Platform, LoadingController } from '@ionic/angular';
+import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/Camera/ngx';
+
+import { File, FileEntry } from '@ionic-native/File/ngx';
+import { HttpClient } from '@angular/common/http';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+
+import { Storage } from '@ionic/storage';
+import { FilePath } from '@ionic-native/file-path/ngx';
+
+import { finalize } from 'rxjs/operators';
+
+const STORAGE_KEY = 'my_images';
 @Component({
   selector: "page-pet-details",
   templateUrl: "pet-details.html",
@@ -21,6 +34,7 @@ export class PetDetailsPage {
   loading: Promise<HTMLIonLoadingElement>;
   rating: number;
   deleting: Promise<HTMLIonLoadingElement>;
+  images = [];
 
   constructor(
     public route: ActivatedRoute,
@@ -30,7 +44,17 @@ export class PetDetailsPage {
     public alertService: AlertService,
     public accountService: AccountService,
     private router: Router,
-    private _location: Location
+    private _location: Location,
+    private camera: Camera,
+    private file: File,
+    private http: HttpClient,
+    private webview: WebView,
+    private actionSheetController: ActionSheetController,
+    private toastController: ToastController,
+    private storage: Storage,
+     private plt: Platform,
+    private loadingController: LoadingController,
+    private ref: ChangeDetectorRef, private filePath: FilePath
   ) {
     this.loading = this.alertService.presentLoading("Pet Check &#10003;");
     this.savingPet = this.alertService.presentLoading("Saving Pet...");
@@ -165,4 +189,64 @@ export class PetDetailsPage {
         },
       });
   }
+
+
+  async startUpload(){
+
+  }
+
+  async deleteImage(){
+
+  }
+
+  async selectImage(){
+
+    const actionSheet = await this.actionSheetController.create({
+      header: "Select Image source",
+      buttons: [{
+              text: 'Load from Library',
+              handler: () => {
+                  //this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+              }
+          },
+          {
+              text: 'Use Camera',
+              handler: () => {
+                 // this.takePicture(this.camera.PictureSourceType.CAMERA);
+              }
+          },
+          {
+              text: 'Cancel',
+              role: 'cancel'
+          }
+      ]
+  });
+  await actionSheet.present();
+
+  }
+
+  /*takePicture(sourceType: PictureSourceType) {
+    var options: CameraOptions = {
+        quality: 100,
+        sourceType: sourceType,
+        saveToPhotoAlbum: false,
+        correctOrientation: true
+    };
+
+    this.camera.getPicture(options).then(imagePath => {
+        if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+            this.filePath.resolveNativePath(imagePath)
+                .then(filePath => {
+                    let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+                    let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+                    this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+                });
+        } else {
+            var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+            var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+        }
+    });*/
+
+
 }
