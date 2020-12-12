7cba100e9cb5b4bf549a526b30927c746d537213
mikecrf121 <cassm.mc@gmail.com>
2020-12-11 22:48:24 -0500

    route renaming singular vs plural

--- client/src/app/pages/reports-manager/reports/report-details/report-details-routing.module.ts
+++ client/src/app/pages/reports-manager/reports/report-details/report-details-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
       ).then((m) => m.StudentDetailsModule),
   },
   {
-    path: "reports-expenses/expense-details/:expenseId",
+    path: "report-expenses/expense-details/:expenseId",
     loadChildren: () =>
       import(
         "@app/pages/reports-manager/expenses/expense-details/expense-details.module"
